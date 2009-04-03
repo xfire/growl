68902cb253242e242101877907aa1307a5cd8f3e
Rico Schiekel <fire@downgra.de>
2009-04-03 23:46:36 +0200

    add some docstrings

--- growl.py
+++ growl.py
@@ -55,24 +55,27 @@ except ImportError:
 
 
 def wrap(orig_func, new_func, name = 'super'):
+    """ helper function to wrap an existing function of a class.
+        e.g.
+    
+        def verbose_write(self):
+            print 'generating post: %s (from: %s)' % (self.title, self.filename)
+            return verbose_write.super(self)
+        Post.write = wrap(Post.write, verbose_write)
+    
+        the original function will be available as <new_function_name>.super.
     """
-    helper function to wrap an existing function of a class.
-    e.g.
 
-    def verbose_write(self):
-        print 'generating post: %s (from: %s)' % (self.title, self.filename)
-        return verbose_write.super(self)
-    Post.write = wrap(Post.write, verbose_write)
-
-    the original function will be available as <new_function_name>.super.
-    """
     @functools.wraps(orig_func)
     def wrapper(*args, **kwargs):
         setattr(new_func, name, orig_func)
         return new_func(*args, **kwargs)
     return wrapper
 
+
 class AttrDict(dict):
+    """ dictionary which provides its items as attributes.
+    """
 
     def __getattr__(self, name):
         return self[name]
@@ -85,6 +88,8 @@ class AttrDict(dict):
 
 
 class Config(object):
+    """ base class providing some static configuration values.
+    """
 
     @classmethod
     def updateconfig(cls, base, deploy):
@@ -99,6 +104,11 @@ class Config(object):
 
 
 class Template(Config):
+    """ abstract template base class providing support for an
+        yaml header, transforming based on file extension,
+        rendering (only using current layout if defined) and 
+        layouting (applying all layouts).
+    """
 
     RE_YAML = re.compile(r'(^---\s*$(?P<yaml>.*?)^---\s*$)?(?P<content>.*)',
                          re.M | re.S)
@@ -112,6 +122,8 @@ class Template(Config):
         self.read_yaml()
 
     def read_yaml(self):
+        """ read yaml header and remove the header from content
+        """
         self._content = file(self.filename, 'r').read()
 
         mo = self.RE_YAML.match(self._content)
@@ -120,11 +132,17 @@ class Template(Config):
             self._content = mo.groupdict().get('content')
 
     def transform(self):
+        """ do transformation based on filename extension.
+            e.g. do markdown or textile transformations
+        """
         ext = os.path.splitext(self.filename)[-1][1:]
         t = self.transformers.get(ext, lambda x: x)
         return t(self._content)
 
     def render(self):
+        """ render content, so transforming and then
+            apply current layout.
+        """
         ctx = self.context.copy()
         ctx.content = renderTemplate(self.transform(), ctx)
         layout = self.layouts.get(ctx.layout)
@@ -134,6 +152,9 @@ class Template(Config):
             return ctx.content
 
     def layout(self):
+        """ layout content, so transforming and then applying
+            all layouts.
+        """
         ctx = self.context.copy()
         ctx.content = self.render()
         layout = self.layouts.get(ctx.layout)
@@ -147,6 +168,8 @@ class Template(Config):
         return ctx.content
 
     def write(self, path, content):
+        """ write content to path in deploy directory.
+        """
         fname = os.path.join(self.DEPLOY_DIR, path)
         dirname = os.path.dirname(fname)
         if not os.path.isdir(dirname):
@@ -167,6 +190,8 @@ class Template(Config):
 
 
 class Layout(Template):
+    """ a layout template from _layouts/ directory.
+    """
 
     def __init__(self, filename, context):
         super(Layout, self).__init__(filename, {}, context)
@@ -185,6 +210,9 @@ class Layout(Template):
 
 
 class Page(Template):
+    """ a page template which should be transformed. e.g. files which
+        filename ends with an '_' or an transformer file extension.
+    """
 
     TRANSFORM = ('_', )
 
@@ -217,12 +245,19 @@ class Page(Template):
 
     @staticmethod
     def transformable(filename):
+        """ return true, if the file is transformable. that means the
+            filename ends with a character from self.TRANSFORM or
+            self.transformers.
+        """
         ext = os.path.splitext(filename)[-1]
         return ((filename[-1] in Page.TRANSFORM) or
                 (ext and ext[1:] in Page.transformers))
 
 
 class Post(Page):
+    """ a post template mapping a single post from the _posts/
+        directory.
+    """
 
     def __init__(self, filename, layout, context):
         super(Post, self).__init__(filename, layout, context)
@@ -255,7 +290,6 @@ class Post(Page):
     @property
     def path(self):
         return os.path.join(self.url, 'index' + self.POST_FILE_EXT)
- 
 
     @property
     def content(self):
@@ -266,6 +300,10 @@ class Post(Page):
 
 
 class Site(Config):
+    """ controls the site and holds the global context object. the context
+        object contains all layouts, all posts and categories.
+        hooks can be used to configure the context.
+    """
 
     CONTEXT = AttrDict()
     IGNORE = ('_', '.')
@@ -288,21 +326,30 @@ class Site(Config):
         self.context.site.now = datetime.datetime.now()
 
     def hooks(self):
+        """ load all available hooks from the _hooks/ directory.
+        """
         if os.path.isdir(self.HOOK_DIR):
             for f in sorted(self.ignoreFilter(os.listdir(self.HOOK_DIR))):
                 if f.endswith('.py'):
                     execfile(os.path.join(self.HOOK_DIR, f), globals())
 
     def read(self):
+        """ read all layouts and posts and calculate the categories.
+        """
         self.read_layouts()
         self.read_posts()
         self.calc_categories()
 
     def generate(self):
+        """ write all posts and then the site content to the deploy directory.
+        """
         self.write_posts()
         self.write_site_content()
 
     def deploy(self):
+        """ empty deply function which can be used by hooks to deploy the
+            generated site. e.g. rsync to the webserver, ...
+        """
         pass
 
     def read_layouts(self):
@@ -388,13 +435,14 @@ class Site(Config):
     def ignoreFilter(self, seq):
         """ filter out files starting with self.IGNORE tokens
         """
+
         def ignore_filter(item):
             for ign in self.IGNORE:
                 if item.startswith(ign):
                     return False
             return True
         return itertools.ifilter(ignore_filter, seq)
-        
+
 
 if __name__ == '__main__':
     DEFAULT_PORT = 8080
