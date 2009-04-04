3eb0d01af5715f24c4bda37279b15360f2b0e1bd
Rico Schiekel <fire@downgra.de>
2009-04-04 19:46:40 +0200

    add deploy via rsync example hook

---
+++ _hooks/deploy_rsync.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# vim:syntax=python:sw=4:ts=4:expandtab
+
+import subprocess
+
+REMOTE_PATH = 'user@host:/path/'
+
+def deploy_rsync(self):
+    cmd = 'rsync -ahz --delete %s/* %s\n' % (self.DEPLOY_DIR, REMOTE_PATH)
+    sys.stderr.write('deploy to >>> %s\n' % REMOTE_PATH)
+    ret = subprocess.call(cmd, shell=True)
+    if ret == 0:
+        sys.stderr.write('<<< finished\n')
+    else:
+        sys.stderr.write('<<< failed! (return code: %d)\n' % ret)
+    return deploy_rsync.super(self)
+
+Site.deploy = wrap(Site.deploy, deploy_rsync)
+
