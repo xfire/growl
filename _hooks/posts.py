0fae0459604013d3db0d4ed7cc4ed1a1fa27a49f
Rico Schiekel <fire@downgra.de>
2009-06-18 22:43:12 +0200

    add first shot of "-n" cmdl parameter to post hook

--- _hooks/posts.py
+++ _hooks/posts.py
@@ -1,5 +1,30 @@
+# vim:syntax=python:sw=4:ts=4:expandtab
+#
+# Copyright (C) 2009 Rico Schiekel (fire at downgra dot de)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 2
+# as published by the Free Software Foundation
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+#
+
 import os
+import time
 import datetime
+import tempfile
+import textwrap
+import readline
+import urllib
+import sha
 
 class Post(Page):
     """ a post template mapping a single post from the _posts/
@@ -80,16 +105,114 @@ class Post(Page):
             for p in self.posts:
                 p.write()
 
+# ----------------------------------------------------------------------------------------
+
+        def get_editor():
+            editor = os.environ.get('GROWL_EDITOR')
+            if not editor:
+                editor = os.environ.get('EDITOR')
+            if not editor:
+                editor = 'vi'
+            return editor
+
+        def launch_editor(content = ''):
+            fn = None
+            try:
+                fid, fn = tempfile.mkstemp('.post', 'growl_', None, True)
+                f = open(fn, 'w')
+                f.write(content)
+                f.close()
+
+                mod_time_start = os.stat(fn).st_mtime
+                rcode = subprocess.call([get_editor(), fn])
+                mod_time_end = os.stat(fn).st_mtime
+
+                hash_org = sha.new(content).digest()
+                f = open(fn, 'r')
+                content = f.read()
+                f.close()
+                hash_new = sha.new(content).digest()
+
+                if rcode != 0 or mod_time_end == mod_time_start or hash_org == hash_new:
+                    return None
+
+                # only delete temp file if anything went ok
+                return content
+            finally:
+                if fn:
+                    os.unlink(fn)
+
+        def raw_input_default(prompt, value = None):
+            if value:
+                readline.set_startup_hook(lambda: readline.insert_text(value))
+            try:
+                return raw_input(prompt)
+            finally:
+                if value:
+                    readline.set_startup_hook(None)
+
+        def mangle_url(url):
+            ou = url
+            url = url.lower()
+            url = ''.join(c for c in url if c not in mangle_url.SP)
+            url = url.replace('&', ' and ')
+            url = url.replace('.', ' dot ')
+            url = url.strip()
+            url = url.replace(' ', '_')
+            return urllib.quote(url)
+        mangle_url.SP = '`~!@#$%^*()+={}[]|\\;:\'",<>/?'
+
         def create_new_post(self):
-            print self.options.new_post
+            TEMPLATE = textwrap.dedent("""
+            ---
+            layout: post
+            title: ???
+            categories: ???
+            ---
+            """).strip()
+            try:
+                content = launch_editor(TEMPLATE)
+                if content:
+                    # load yaml header
+                    mo = Template.RE_YAML.match(content)
+                    if mo and mo.groupdict().get('yaml'):
+                        meta = yaml.load(mo.groupdict().get('yaml'))
+                        title = meta.get('title')
+
+                    if title:
+                        title = mangle_url(title)
+
+                        filename = time.strftime('%Y-%m-%d-', datetime.datetime.now().timetuple())
+                        filename += title
+
+                        try:
+                            filename = raw_input_default('filename: ', filename)
+                            filename = os.path.join(self.POST_DIR, filename)
+                            f = open(filename, 'w')
+                            f.write(content)
+                            f.close()
+                            print 'created post: %s' % filename
+                        except KeyboardInterrupt:
+                            # save backup to temp file
+                            print '\nabort...'
+                            fid, fn = tempfile.mkstemp('.post', 'growl_', None, True)
+                            f = open(fn, 'w')
+                            f.write(content)
+                            f.close()
+                else:
+                    print 'abort...'
+            except Exception, e:
+                print 'can\'t create new post: %s' % e
 
         @wrap(clazz.setupOptions)
         def setupOptions(forig, self, parser):
             forig(self, parser)
             parser.add_option('-n', '--newpost',
-                              action = 'store', dest = 'new_post',
-                              metavar = 'TITLE',
+                              action = 'store_true', dest = 'new_post',
                               help = 'create new post')
+
+# ----------------------------------------------------------------------------------------
+
 
         @wrap(clazz.prepare)
         def site_prepare(forig, self):
